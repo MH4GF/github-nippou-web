89f0c9ea33284b69c19305c060b61a1cf183ff1f
MH4GF <h.miyagi.cnw@gmail.com>
2023-10-16 08:37:41 +0900

    feat: Authorizationヘッダでトークンを渡す

--- web-deno/index.ts
+++ web-deno/index.ts
@@ -3,21 +3,29 @@ import { Hono } from "https://deno.land/x/hono@v3.7.6/mod.ts";
 
 import "./wasm_exec.js";
 
+const buf = await Deno.readFile("./web-deno/main.wasm");
+
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello! Hono!"));
+app.get("/", async (c) => {
+  // console.log({ header: c.req.raw.headers });
+  const token = c.req.header("Authorization");
+  if (token === undefined) {
+    return c.json({ error: "no token" }, 401);
+  }
+  // @ts-ignore: todo
+  const go = new Go();
+  const { instance } = await WebAssembly.instantiate(buf, go.importObject);
+  go.env["GITHUB_NIPPOU_USER"] = "MH4GF";
+  go.env["GITHUB_NIPPOU_ACCESS_TOKEN"] = token;
+  go.run(instance);
+  // @ts-ignore: todo
+  const result = await GithubNippou().catch((e) => {
+    console.error("error:", e);
+    return c.json({ error: e }, 500);
+  });
 
-// @ts-ignore: todo
-const go = new Go();
-const buf = await Deno.readFile("./web-deno/main.wasm");
-const { instance } = await WebAssembly.instantiate(buf, go.importObject);
-go.env["GITHUB_NIPPOU_USER"] = "MH4GF";
-go.env["GITHUB_NIPPOU_ACCESS_TOKEN"] = Deno.env.get(
-  "GITHUB_NIPPOU_ACCESS_TOKEN"
-);
-go.run(instance);
-
-// @ts-ignore: todo
-GithubNippou();
+  return c.json({ result });
+});
 
 serve(app.fetch);

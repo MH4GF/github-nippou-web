519f4cd35fe1d46f741a5b7ba304ff27788de499
MH4GF <h.miyagi.cnw@gmail.com>
2023-10-16 10:03:08 +0900

    GoのWebサーバーで動かす

---
+++ api/go.mod
@@ -0,0 +1,19 @@
+module mh4gf/github-nippou-web
+
+go 1.21.2
+
+require github.com/MH4GF/github-nippou v1.2.1-0.20231016010059-1720dc594c5c
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rakyll/statik v0.1.7 // indirect
+	github.com/skratchdot/open-golang v0.0.0-20190402232053-79abb63cd66e // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/oauth2 v0.12.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/MH4GF/github-nippou/lib"
+)
+
+type ResponseBody struct {
+	Result string `json:"result"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	debug := true
+	nowDate := time.Now().Format("20060102")
+	sinceDate := nowDate
+	untilDate := nowDate
+	auth := lib.Auth{
+		User:        "MH4GF",
+		AccessToken: os.Getenv("GITHUB_NIPPOU_ACCESS_TOKEN"),
+	}
+
+	lines, err := lib.List(sinceDate, untilDate, debug, auth)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	body := ResponseBody{Result: lines}
+	fmt.Println(body)
+
+	jsonData, err := json.Marshal(body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// レスポンスヘッダーの設定
+	w.Header().Set("Content-Type", "application/json")
+
+	// JSONデータをクライアントに送信
+	w.Write(jsonData)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}

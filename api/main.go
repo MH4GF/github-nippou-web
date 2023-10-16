0ab421d7cb6c4e96fb4d0804855ffb9816b2bb87
MH4GF <h.miyagi.cnw@gmail.com>
2023-10-16 10:12:14 +0900

    fix: portを環境変数から取得

--- api/main.go
+++ api/main.go
@@ -47,5 +47,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(os.Getenv("PORT"), nil)
 }

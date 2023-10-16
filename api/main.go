76affbb609a9ced7c1c5683dba3e217942cb04e6
MH4GF <h.miyagi.cnw@gmail.com>
2023-10-16 22:27:34 +0900

    fix: 負荷緩和のため、Cache-Controlを60秒設定する

--- api/main.go
+++ api/main.go
@@ -44,6 +44,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 	// レスポンスヘッダーの設定
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "max-age=60")
 
 	// JSONデータをクライアントに送信
 	w.Write(jsonData)

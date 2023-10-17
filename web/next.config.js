d568ca54ee6585445a80349833c6be0ffbe8aac9
MH4GF <h.miyagi.cnw@gmail.com>
2023-10-17 09:36:06 +0900

    remove unused wasm code

--- web/next.config.js
+++ web/next.config.js
@@ -1,27 +1,7 @@
-const CopyPlugin = require("copy-webpack-plugin");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: true,
-  },
-  webpack: (config, { isServer }) => {
-    console.log("call webpack", { isServer });
-
-    if (isServer) {
-      config.plugins.push(
-        new CopyPlugin({
-          patterns: [
-            {
-              from: "src/app/server-actions/main.wasm",
-              to: "./app/server-actions/main.wasm",
-            },
-          ],
-        })
-      );
-    }
-
-    return config;
   },
 };
 
--- web/src/app/server-actions/showList.ts
+++ web/src/app/server-actions/showList.ts
@@ -1,24 +1,8 @@
 "use server";
 
-import "./wasm_exec";
-import { promises as fs } from "fs";
-import path from "path";
-import { getServerSession } from "../_auth/getServerSession";
-
 export const showList = async () => {
   console.log("showList");
-  const session = await getServerSession();
+  // const session = await getServerSession();
 
-  // @ts-ignore
-  const go = new Go();
-  const buf = await fs.readFile(path.join(__dirname, "./main.wasm"));
-  const { instance } = await WebAssembly.instantiate(buf, go.importObject);
-  go.env["GITHUB_NIPPOU_USER"] = "MH4GF";
-  go.env["GITHUB_NIPPOU_ACCESS_TOKEN"] = session?.user.accessToken;
-  go.run(instance);
-
-  // @ts-ignore
-  const ret = await global.GithubNippou();
-
-  return { result: ret };
+  return { result: "" };
 };

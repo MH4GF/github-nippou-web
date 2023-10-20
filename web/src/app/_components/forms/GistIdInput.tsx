aca964d5e09dbbbd9b540d85f2e51bdcf0634e3f
MH4GF <h.miyagi.cnw@gmail.com>
2023-10-20 09:16:23 +0900

    fix: fix typo settingsGistId

--- web/src/app/_components/forms/GistIdInput.tsx
+++ web/src/app/_components/forms/GistIdInput.tsx
@@ -7,13 +7,13 @@ export const GistIdInput = () => {
 
   return (
     <div>
-      <label htmlFor="settingGistId" className="block text-sm font-medium leading-6 text-gray-900">
+      <label htmlFor="settingsGistId" className="block text-sm font-medium leading-6 text-gray-900">
         Settings Gist ID(for customizing output format)
       </label>
       <div className="mt-2">
         <input
-          name="settingGistId"
-          id="settingGistId"
+          name="settingsGistId"
+          id="settingsGistId"
           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
           defaultValue={gistId}
           onChange={(e) => setGistId(e.target.value)}
--- web/src/app/showList.ts
+++ web/src/app/showList.ts
@@ -6,15 +6,15 @@ export const showList = async (_prevState: any, formData: FormData) => {
   const session = await getServerSession()
   const user = session?.user.login
   const token = session?.user.accessToken
-  const settingGistId = formData.get('settingGistId')
-  if (user === undefined || token === undefined || typeof settingGistId !== 'string') {
+  const settingsGistId = formData.get('settingsGistId')
+  if (user === undefined || token === undefined || typeof settingsGistId !== 'string') {
     return { result: 'error' }
   }
 
   const url = new URL(process.env.API_URL)
   url.searchParams.set('user', user)
   url.searchParams.set('token', token)
-  url.searchParams.set('setting_gist_id', settingGistId)
+  url.searchParams.set('settings_gist_id', settingsGistId)
   const response = await fetch(url)
   const json = await response.json()
   return { result: json.result }

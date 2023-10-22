0087c47cb001c66d3f01468a348a2d354a754f8d
MH4GF <h.miyagi.cnw@gmail.com>
2023-10-22 19:19:22 +0900

    feat: implement copy to clipboard button

---
+++ web/src/app/_features/CopyToClipboardButton/CopyToClipboardButton.tsx
@@ -0,0 +1,34 @@
+import type { FC } from 'react'
+import { useCallback, useState } from 'react'
+
+import { Tooltip } from './Tooltip'
+import { useSuccessTooltip } from './useSuccessTooltip'
+
+import { Button } from '@/app/_components'
+
+interface Props {
+  text: string
+}
+
+export const CopyToClipboardButton: FC<Props> = ({ text }) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [showTooltip, setShowTooltip] = useSuccessTooltip()
+
+  const handleClick = useCallback(() => {
+    setIsLoading(true)
+    navigator.clipboard
+      .writeText(text)
+      .then(() => setShowTooltip(true))
+      .catch((e) => console.error(e))
+    setIsLoading(false)
+  }, [text, setShowTooltip])
+
+  return (
+    <span className="relative">
+      <Tooltip show={showTooltip} />
+      <Button isLoading={isLoading} onClick={handleClick} variant="secondary">
+        Copy to Clipboard
+      </Button>
+    </span>
+  )
+}
---
+++ web/src/app/_features/CopyToClipboardButton/useSuccessTooltip.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+
+export const useSuccessTooltip = () => {
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    if (!show) return
+
+    const timer = setTimeout(() => {
+      setShow(false)
+    }, 2000)
+    return () => clearTimeout(timer)
+  }, [show])
+
+  return [show, setShow] as const
+}

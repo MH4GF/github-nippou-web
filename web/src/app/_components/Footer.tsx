3f33fa2d694cccf3e3d99d4de2c49cee0e2daffe
MH4GF <h.miyagi.cnw@gmail.com>
2023-11-26 17:01:04 +0900

    feat: add footer

---
+++ web/src/app/_components/Footer.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+import { GitHubStarButton } from './GitHubStarButton'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p className="inline-flex w-full justify-center gap-2">
+        <span>
+          Built by&nbsp;
+          <Link
+            href="https://twitter.com/MH4GF"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            @MH4GF
+          </Link>
+          .
+        </span>
+        <GitHubStarButton />
+      </p>
+    </footer>
+  )
+}
---
+++ web/src/app/_components/GitHubStarButton.tsx
@@ -0,0 +1,13 @@
+import GitHubButton from 'react-github-btn'
+
+export const GitHubStarButton = () => {
+  return (
+    <GitHubButton
+      href="https://github.com/MH4GF/github-nippou-web"
+      data-show-count="true"
+      aria-label="Star MH4GF/github-nippou-web on GitHub"
+    >
+      Star
+    </GitHubButton>
+  )
+}

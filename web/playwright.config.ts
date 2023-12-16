990116bf24792c60d8b2c361acb96b3390860755
MH4GF <h.miyagi.cnw@gmail.com>
2023-12-16 22:52:58 +0900

    feat: setup playwright for vrt

---
+++ web/playwright.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './src/app/__vrt__',
+  testMatch: '*.vrt.ts',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: process.env.BASE_URL ?? 'https://github-nippou-web.vercel.app/',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+  ],
+})
---
+++ web/src/__vrt__/page.vrt.ts
@@ -0,0 +1,29 @@
+import path from 'path'
+
+import { test, type Page, type TestInfo } from '@playwright/test'
+
+interface TargetPage {
+  name: string
+  path: string
+}
+
+const screenshot = async (page: Page, testInfo: TestInfo, targetPage: TargetPage) => {
+  const fileName = `${targetPage.name}.png`
+  console.log({ testInfo })
+  await page.goto(targetPage.path)
+  await page.screenshot({
+    fullPage: true,
+    path: path.join(testInfo.project.snapshotDir, 'snapshots', fileName),
+  })
+}
+
+const targetPage: TargetPage = {
+  name: 'home',
+  path: '/',
+}
+
+test.describe('screenshot', () => {
+  test(targetPage.name, async ({ page }, testInfo) => {
+    await screenshot(page, testInfo, targetPage)
+  })
+})

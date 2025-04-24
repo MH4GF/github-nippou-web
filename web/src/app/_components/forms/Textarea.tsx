b9d8674fde7c2e3eca17d8bb187d244d5bd4b400
MH4GF <h.miyagi.cnw@gmail.com>
2025-04-24 22:55:05 +0900

    refactor: update Tailwind CSS to v4 configuration

--- web/src/app/_components/forms/Textarea.tsx
+++ web/src/app/_components/forms/Textarea.tsx
@@ -7,7 +7,7 @@ export const Textarea: FC<Props> = (props) => {
     <textarea
       {...props}
       className={`
-      block w-full rounded-md border-0 p-1.5 text-slate-900
+      block w-full rounded-md border-0 p-1.5 text-slate-900 bg-white
       shadow-sm ring-1 ring-inset ring-slate-300 placeholder:text-slate-400
       focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6`}
     />
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms'
 
 const config: Config = {
   content: [
@@ -14,6 +15,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [forms],
 }
 export default config
